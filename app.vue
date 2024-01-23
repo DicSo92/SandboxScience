c3234da8032afb442e454654bce5c48088da9927
Luzzi Charly <luzzi.charly@gmail.com>
2024-01-23 18:08:15 +0100

    Setup first route

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import { appName } from '~/constants'
+
+useHead({
+  title: appName,
+})
+</script>
+
+<style>
+html, body, #__nuxt {
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+
+html.dark {
+  background: #222;
+  color: white;
+}
+</style>
---
+++ constants/index.ts
@@ -0,0 +1,2 @@
+export const appName = 'Game Of Life'
+export const appDescription = 'Original Game Of Life by .... made with Javascript'
