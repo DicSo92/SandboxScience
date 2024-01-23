63d3a08ab0511aeb79831611297e5cb1a0b7fd05
Luzzi Charly <luzzi.charly@gmail.com>
2024-01-23 18:46:10 +0100

    Install unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: {enabled: true},
+    devtools: { enabled: true },
     modules: [
         '@vueuse/nuxt',
+        '@unocss/nuxt',
         ['@pinia/nuxt', {autoImports: ['defineStore']}],
 
     ],
+
+    css: [
+        '@unocss/reset/tailwind.css', // Maybe need @unocss/reset package
+    ],
+    features: {
+        // For UnoCSS
+        inlineStyles: false,
+    },
+
     alias: {
         pinia: process.env.NODE_ENV === 'production' ? '/node_modules/pinia/dist/pinia.mjs' : '/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs',
     },
