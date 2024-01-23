93c4678fb31638755e025390a66690c25f4b2f48
Luzzi Charly <luzzi.charly@gmail.com>
2024-01-23 18:21:54 +0100

    Install pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    ['@pinia/nuxt', { autoImports: ['defineStore'] }],
+  ],
+  alias: {
+    pinia: process.env.NODE_ENV === 'production' ? '/node_modules/pinia/dist/pinia.mjs' : '/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs',
+  },
 })
