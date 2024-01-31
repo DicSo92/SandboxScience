3b5cd0fb1a50533b0b581cf20b89f9568812aa02
Luzzi Charly <luzzi.charly@gmail.com>
2024-01-31 02:22:42 +0100

    Create SelectMenu

--- components/GameView.vue
+++ components/GameView.vue
@@ -12,127 +12,150 @@
                 <div mx-2>Execution Time: {{ Math.round(executionTime) }} ms</div>
             </div>
             <div flex>
-                <div btn @click="randomCells(((game.rows * game.cols) * 20) / 100)" class="bg-orange-700 hover:bg-orange-800 p2 mx-1">Random</div>
-                <div btn @click="killRandom(20)" class="bg-red-700 hover:bg-red-900 p2 mx-1">Kill</div>
-                <div btn @click="newCycle" class="bg-green-900 hover:bg-green-800 p2 mx-1">Cycle</div>
-                <div btn @click="startLoop" class="bg-green-700 hover:bg-green-900 flex items-center mx-1">
+                <div btn p2 mx-1 bg="orange-700 hover:orange-800" @click="randomCells(((game.rows * game.cols) * 20) / 100)">Random Cells</div>
+                <div btn p2 mx-1 bg="red-700 hover:red-900" @click="killRandom(20)">Random Kills</div>
+                <div btn p2 mx-1 bg="green-900 hover:green-800" @click="newCycle">Step</div>
+                <div btn flex items-center mx-1
+                     bg="green-700 hover:green-900"
+                     @click="startLoop">
                     <div i-carbon-play mr-1></div>
-                    Start Loop
+                    Start
+                </div>
+                <div btn flex items-center mx-1
+                     bg="blue-700 hover:blue-900"
+                     @click="pause">
+                    <div i-carbon-pause mr-1></div>
+                    Pause
                 </div>
             </div>
+            <SelectMenu label="Edge Mode :"
+                        :options="[
+                            { name: 'Mirror Edges', icon: 'i-carbon-compare', id: 'mirror'},
+                            { name: 'Dead Edges', icon: 'i-carbon-compare', id: 'dead'},
+                            { name: 'Alive Edges', icon: 'i-carbon-compare', id: 'alive'}]"
+                        :selected="game.edgeMode"
+                        @selected="(id) => game.edgeMode = id"
+            />
         </div>
     </section>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue'
+import { defineComponent } from 'vue'
 import { aliveNeighbours } from '~/helpers/utils/neighbours'
 import { Cell } from '~/models/classes/Cell'
 import type { ICell } from '~/models/interfaces/ICell.interface'
+import SelectMenu from "~/components/SelectMenu.vue";
 
 export default defineComponent({
-        setup() {
-            const game = useGameStore()
-            const canvas: Ref<HTMLCanvasElement | undefined> = ref()
-            const ctx: Ref<CanvasRenderingContext2D | undefined> = ref()
+    components: { SelectMenu },
+    setup() {
+        const game = useGameStore()
+        const canvas: Ref<HTMLCanvasElement | undefined> = ref()
+        const ctx: Ref<CanvasRenderingContext2D | undefined> = ref()
 
-            const executionTime = ref<number>(0) // cycle execution time
-            let startExecutionTime: number // for calculating execution time
-            let lastTime: number | null // for calculating elapsed time
+        const executionTime = ref<number>(0) // cycle execution time
+        let startExecutionTime: number // for calculating execution time
+        let lastTime: number | null // for calculating elapsed time
 
-            onMounted(() => {
-                ctx.value = canvas.value?.getContext('2d') || undefined
-                drawCanvas()
+        onMounted(() => {
+            ctx.value = canvas.value?.getContext('2d') || undefined
+            drawCanvas()
+        })
+
+        function drawCanvas() {
+            canvas.value!.width = game.canvasWidth
+            canvas.value!.height = game.canvasHeight
+
+            for (let y = 0; y < game.rows; y++) {
+                for (let x = 0; x < game.cols; x++) {
+                    let index = x + y * game.cols
+                    game.cellsArray[index] = reactive(new Cell(x, y, game.size, ctx.value))
+                }
+            }
+            console.log(game.cellsArray)
+        }
+
+        const randomCells = (num: number) => {
+            for (let i = 0; i < num; i++) {
+                const random = Math.floor(Math.random() * game.cellsArray.length)
+                game.cellsArray[random].makeAlive(true)
+            }
+            console.log(game.cellsArray)
+        }
+        const killRandom = (num: number) => {
+            const shuffled = [...game.cellsArray].sort(() => 0.5 - Math.random())
+            const rCells = shuffled.slice(0, num)
+            rCells.forEach((cell) => {
+                cell.kill(true)
             })
+        }
 
-            function drawCanvas() {
-                canvas.value!.width = game.canvasWidth
-                canvas.value!.height = game.canvasHeight
-
-                for (let y = 0; y < game.rows; y++) {
-                    for (let x = 0; x < game.cols; x++) {
-                        let index = x + y * game.cols
-                        game.cellsArray[index] = reactive(new Cell(x, y, game.size, ctx.value))
-                    }
-                }
-                console.log(game.cellsArray)
+        function startLoop() {
+            lastTime = Number(document.timeline.currentTime) || null
+            if (!lastTime) {
+                console.log("Can't get document.timeline.currentTime")
+                return
             }
 
-            const randomCells = (num: number) => {
-                for (let i = 0; i < num; i++) {
-                    const random = Math.floor(Math.random() * game.cellsArray.length)
-                    game.cellsArray[random].makeAlive(true)
-                }
-                console.log(game.cellsArray)
-            }
-            const killRandom = (num: number) => {
-                const shuffled = [...game.cellsArray].sort(() => 0.5 - Math.random())
-                const rCells = shuffled.slice(0, num)
-                rCells.forEach((cell) => {
-                    cell.kill(true)
-                })
-            }
+            lastTime = lastTime - game.SPEED // to start a cycle immediately
+            startExecutionTime = performance.now()
 
-            function startLoop() {
-                lastTime = Number(document.timeline.currentTime) || null
-                if (!lastTime) {
-                    console.log("Can't get document.timeline.currentTime")
-                    return
-                }
+            game.isRunning = true
+            requestAnimationFrame(animate)
+        }
+        function pause() {
+            game.isRunning = false
+        }
 
-                lastTime = lastTime - game.SPEED // to start a cycle immediately
-                startExecutionTime = performance.now()
-                requestAnimationFrame(animate)
+
+        function animate(currentTime: number) {
+            const elapsedTime = currentTime - lastTime!
+
+            if (elapsedTime >= game.SPEED && game.isRunning) {
+                newCycle()
+
+                lastTime = currentTime - (elapsedTime % game.SPEED)
+                executionTime.value = performance.now() - startExecutionTime
+                console.log(`Execution Time : ${executionTime.value} ms`)
             }
 
+            startExecutionTime = performance.now()
+            requestAnimationFrame(animate);
+        }
 
-            function animate(currentTime: number) {
-                const elapsedTime = currentTime - lastTime!
+        function newCycle() {
+            console.log(game.cellsArray)
+            let changedCells = [] as ICell[]
+            game.cellsArray.forEach((cell, index) => {
+                const hasChanged = processRules(cell, aliveNeighbours(cell.x, cell.y))
+                if (hasChanged) changedCells.push(cell)
+            })
+            changedCells.forEach((cell, index) => {
+                cell.isAlive = cell.nextAlive
+            })
+        }
 
-                if (elapsedTime >= game.SPEED) {
-                    newCycle()
-
-                    lastTime = currentTime - (elapsedTime % game.SPEED)
-                    executionTime.value = performance.now() - startExecutionTime
-                    console.log(`Execution Time : ${executionTime.value} ms`)
-                }
-
-                startExecutionTime = performance.now()
-                requestAnimationFrame(animate);
+        function processRules(cell: ICell, aliveNeighbours: number): boolean { // return if cell has changed
+            if (cell.isAlive && (aliveNeighbours === 2 || aliveNeighbours === 3)) {
+                // Survives
+                return false
+            } else if (!cell.isAlive && aliveNeighbours === 3) {
+                // Born
+                cell.makeAlive()
+                return true
+            } else {
+                // Dies
+                if (cell.isAlive) cell.kill()
+                return true
             }
+        }
 
-            function newCycle() {
-                console.log(game.cellsArray)
-                let changedCells = [] as ICell[]
-                game.cellsArray.forEach((cell, index) => {
-                    const hasChanged = processRules(cell, aliveNeighbours(cell.x, cell.y))
-                    if (hasChanged) changedCells.push(cell)
-                })
-                changedCells.forEach((cell, index) => {
-                    cell.isAlive = cell.nextAlive
-                })
-            }
-
-            function processRules(cell: ICell, aliveNeighbours: number): boolean { // return if cell has changed
-                if (cell.isAlive && (aliveNeighbours === 2 || aliveNeighbours === 3)) {
-                    // Survives
-                    return false
-                } else if (!cell.isAlive && aliveNeighbours === 3) {
-                    // Born
-                    cell.makeAlive()
-                    return true
-                } else {
-                    // Dies
-                    if (cell.isAlive) cell.kill()
-                    return true
-                }
-            }
-
-            return {
-                game, ctx, canvas, newCycle, randomCells, killRandom, startLoop, executionTime
-            }
-        },
-    })
+        return {
+            game, ctx, canvas, newCycle, randomCells, killRandom, startLoop, pause, executionTime
+        }
+    }
+})
 
 </script>
 
--- stores/gameStore.ts
+++ stores/gameStore.ts
@@ -4,13 +4,14 @@ import { Cell } from "~/models/classes/Cell"
 export const useGameStore = defineStore('game', () => {
     const canvasWidth = ref<number>(2000) // the width of the canvas
     const canvasHeight = ref<number>(1000) // the height of the canvas
-    const size = ref<number>(10) // the size of every cell
+    const size = ref<number>(20) // the size of every cell
     const rows = ref<number>(canvasHeight.value / size.value) // number of rows
     const cols = ref<number>(canvasWidth.value / size.value) // number of columns
 
     const SPEED = ref<number>(50) // the speed of the animation (ms)
     const cellsArray = reactive([] as Cell[]) // array of cells
     const edgeMode = ref<string>('mirror') // dead, alive, mirror
+    const isRunning = ref<boolean>(false) // is the animation running?
 
-    return { SPEED, canvasWidth, canvasHeight, size, rows, cols, cellsArray, edgeMode }
+    return { SPEED, canvasWidth, canvasHeight, size, rows, cols, cellsArray, edgeMode, isRunning }
 })
