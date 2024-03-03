79c39c57475e3857ea49bb307f9419291ab5d456
Luzzi Charly <luzzi.charly@gmail.com>
2024-03-03 22:30:06 +0100

    Make it 3D (AWESOOOME)

--- pages/particle-life.vue
+++ pages/particle-life.vue
@@ -15,24 +15,26 @@ export default defineComponent({
 
         const numParticles: number = 2000
         const particleSize: number = 2
-        const numColors: number = 6
+        const numColors: number = 8
+        const is3D: boolean = true
 
         const colorList: string[] = ['yellow', 'red', 'green', 'cyan', 'magenta', 'blue', 'white', 'orange', 'purple', 'pink']
         let currentColors: number[] = []
         let rulesMatrix: number[][] = []
 
-        let maxRadius: number = 40 // maximum distance for particles to start attracting
+        let maxRadius: number = 60 // maximum distance for particles to start attracting
         let minRadius: number = 20 // minimum distance for particles to start repelling
         let repel: number = 1 // repel force for particles that are too close to each other
         let forceFactor: number = 0.4 // Decrease will increase the impact of the force on the velocity
-        let frictionFactor: number = 0.5 // Slow down the particles (0 to 1, where 1 is no friction)
+        let frictionFactor: number = 0.6 // Slow down the particles (0 to 1, where 1 is no friction)
 
         let colors = new Int32Array(numParticles)
         let positionX = new Float32Array(numParticles)
         let positionY = new Float32Array(numParticles)
+        let positionZ = new Float32Array(numParticles)
         let velocityX = new Float32Array(numParticles).fill(0)
         let velocityY = new Float32Array(numParticles).fill(0)
-
+        let velocityZ = new Float32Array(numParticles).fill(0)
 
         onMounted(() => {
             ctx = canvasRef.value?.getContext('2d') || undefined
@@ -58,6 +60,7 @@ export default defineComponent({
                 colors[i] = currentColors[Math.floor(Math.random() * numColors)]
                 positionX[i] = Math.random() * canvasWidth
                 positionY[i] = Math.random() * canvasHeight
+                positionZ[i] = Math.random() * 5
             }
         }
         function makeRandomMatrix() {
@@ -70,9 +73,11 @@ export default defineComponent({
             }
             return matrix
         }
-        function draw(x: number, y: number, color: number, size: number) {
+        function draw(x: number, y: number, z: number, color: number, size: number) {
             ctx!.fillStyle = colorList[color]
-            ctx!.fillRect(x, y, size, size)
+            const depthFactor = 1 - z / canvasHeight // Adjust this factor to control the depth effect
+            const newSize = size * depthFactor
+            ctx!.fillRect(x, y, newSize, newSize)
         }
         function getForce(ruleFactor: number, distance: number) {
             if (distance < minRadius) {
@@ -87,22 +92,33 @@ export default defineComponent({
         }
         function processRules() {
             for (let i = 0; i < numParticles; i++) {
+                const posXA = positionX[i]
+                const posYA = positionY[i]
+                const posZA = positionZ[i]
                 for (let j = 0; j < numParticles; j++) {
                     if (i === j) continue
-                    let distanceX = Math.abs(positionX[i] - positionX[j])
-                    let distanceY = Math.abs(positionY[i] - positionY[j])
-                    const distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY)
+                    const posXB = positionX[j]
+                    const posYB = positionY[j]
+                    const posZB = positionZ[j]
+
+                    let distanceX = Math.abs(posXA - posXB)
+                    let distanceY = Math.abs(posYA - posYB)
+                    let distanceZ = Math.abs(posZA - posZB)
+                    const distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY + distanceZ * distanceZ)
                     if (distance < maxRadius) {
                         const force = getForce(rulesMatrix[colors[i]][colors[j]], distance)
 
-                        let dx = (positionX[j] - positionX[i])
-                        let dy = (positionY[j] - positionY[i])
+                        distanceX = (posXB - posXA)
+                        distanceY = (posYB - posYA)
+                        distanceZ = (posZB - posZA)
 
-                        let cos = dx / distance
-                        let sin = dy / distance
+                        const cos = distanceX / distance
+                        const sin = distanceY / distance
+                        const tan = distanceZ / distance
 
                         velocityX[i] += cos * force * (1 / forceFactor)
                         velocityY[i] += sin * force * (1 / forceFactor)
+                        velocityZ[i] += tan * force * (1 / forceFactor)
                     }
                 }
             }
@@ -112,9 +128,12 @@ export default defineComponent({
             for (let i = 0; i < numParticles; i++) {
                 velocityX[i] *= frictionFactor
                 velocityY[i] *= frictionFactor
+                velocityZ[i] *= frictionFactor
                 positionX[i] += velocityX[i]
                 positionY[i] += velocityY[i]
-                draw(positionX[i], positionY[i], currentColors[colors[i]], particleSize)
+                positionZ[i] += velocityZ[i]
+
+                draw(positionX[i], positionY[i], positionZ[i], currentColors[colors[i]], particleSize)
             }
         }
         function update() {
@@ -133,6 +152,6 @@ export default defineComponent({
 
 <style scoped>
 canvas {
-    background: #0b0b0b;
+    background: black;
 }
 </style>
