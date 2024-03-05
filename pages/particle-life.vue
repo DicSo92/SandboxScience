4c382c31cd06f5cf69834db292b1c461a3f40c05
Luzzi Charly <luzzi.charly@gmail.com>
2024-03-05 02:32:51 +0100

    CenterView / opacity depth effect / optimize drawing && refactor code

--- pages/particle-life.vue
+++ pages/particle-life.vue
@@ -13,42 +13,46 @@ export default defineComponent({
         let canvasWidth: number = 0
         let canvasHeight: number = 0
 
-        const numParticles: number = 4000
-        const particleSize: number = 2
-        const numColors: number = 10
-        const randomZDepth: number = 5
-        const minZ: number = -180
-        const maxZ:number = 180
-
         const colorList: string[] = ['yellow', 'red', 'green', 'cyan', 'magenta', 'blue', 'white', 'orange', 'purple', 'pink']
         let currentColors: number[] = []
         let rulesMatrix: number[][] = []
 
+        const numParticles: number = 8000 // Number of particles
+        const particleSize: number = 4 // Size of the particles at zoomFactor = 1
+        const numColors: number = 8 // Number of colors to be used
+        const depthLimit: number = 240 // Maximum Z axis depth (0 means almost 2D because there is friction with the walls)
+
+        const minZDepthRandomParticle: number = depthLimit * 0.2 // Particles will be drawn in the last X% of the depth
+        const maxZDepthRandomParticle: number = depthLimit * 0.45 // Particles will be drawn in the first X% of the depth
+        const screenMultiplierForGridSize: number = 2.5 // Multiplier for the grid size based on the screen size
+
         let maxRadius: number = 60 // maximum distance for particles to start attracting
         let minRadius: number = 20 // minimum distance for particles to start repelling
         let repel: number = 1 // repel force for particles that are too close to each other
-        let forceFactor: number = 0.4 // Decrease will increase the impact of the force on the velocity
+        let forceFactor: number = 0.4 // Decrease will increase the impact of the force on the velocity (the higher the value, the slower the particles will move)
         let frictionFactor: number = 0.6 // Slow down the particles (0 to 1, where 1 is no friction)
+        let zoomFactor: number = 1 // Zoom level
 
-        let colors = new Int32Array(numParticles)
-        let positionX = new Float32Array(numParticles)
-        let positionY = new Float32Array(numParticles)
-        let positionZ = new Float32Array(numParticles)
-        let velocityX = new Float32Array(numParticles).fill(0)
-        let velocityY = new Float32Array(numParticles).fill(0)
-        let velocityZ = new Float32Array(numParticles).fill(0)
+        let gridOffsetX: number = 0 // Grid offset X
+        let gridOffsetY: number = 0 // Grid offset Y
+        let gridWidth: number = 0 // Grid width
+        let gridHeight: number = 0 // Grid height
+        let endGridX: number = 0 // Position X of the end of the grid
+        let endGridY: number = 0 // Position Y of the end of the grid
 
-        let pointerX: number = 0
-        let pointerY: number = 0
-        let isDragging: boolean = false
-        let cameraOffsetX: number = 0
-        let cameraOffsetY: number = 0
-        let lastPointerX: number = 0
-        let lastPointerY: number = 0
-        let zoomFactor: number = 1
+        let isDragging: boolean = false // Flag to check if the mouse is being dragged
+        let lastPointerX: number = 0 // For dragging
+        let lastPointerY: number = 0 // For dragging
+        let pointerX: number = 0 // Pointer X
+        let pointerY: number = 0 // Pointer Y
 
-        let endHorizontalX = 0
-        let endVerticalY = 0
+        let colors = new Int32Array(numParticles) // Color of each particle
+        let positionX = new Float32Array(numParticles) // X position of each particle
+        let positionY = new Float32Array(numParticles) // Y position of each particle
+        let positionZ = new Float32Array(numParticles) // Z position of each particle
+        let velocityX = new Float32Array(numParticles).fill(0) // X velocity of each particle
+        let velocityY = new Float32Array(numParticles).fill(0) // Y velocity of each particle
+        let velocityZ = new Float32Array(numParticles).fill(0) // Z velocity of each particle
 
         onMounted(() => {
             ctx = lifeCanvas.value?.getContext('2d') || undefined
@@ -91,8 +95,8 @@ export default defineComponent({
         }
         function handleMove() {
             if (isDragging) {
-                cameraOffsetX += (pointerX - lastPointerX) / zoomFactor
-                cameraOffsetY += (pointerY - lastPointerY) / zoomFactor
+                gridOffsetX += (pointerX - lastPointerX) / zoomFactor
+                gridOffsetY += (pointerY - lastPointerY) / zoomFactor
                 lastPointerX = pointerX
                 lastPointerY = pointerY
                 setEndCoordinates()
@@ -102,20 +106,35 @@ export default defineComponent({
             const oldZoomFactor = zoomFactor
             const zoomIntensity = 0.1
             const zoomDelta = delta * zoomIntensity
-            zoomFactor = Math.max(0.2, Math.min(10, zoomFactor + zoomDelta))
+            zoomFactor = Math.max(0.1, Math.min(3.2, zoomFactor + zoomDelta))
 
-            cameraOffsetX -= x * ((zoomFactor / oldZoomFactor) - 1)
-            cameraOffsetY -= y * ((zoomFactor / oldZoomFactor) - 1)
+            gridOffsetX -= x * ((zoomFactor / oldZoomFactor) - 1)
+            gridOffsetY -= y * ((zoomFactor / oldZoomFactor) - 1)
 
             console.log(zoomFactor)
             setEndCoordinates()
         }
         // -------------------------------------------------------------------------------------------------------------
         function initLife() {
+            // Set the grid size and zoom factor based on the screen size
+            gridWidth = Math.floor(canvasWidth * screenMultiplierForGridSize)
+            gridHeight = Math.floor(canvasHeight * screenMultiplierForGridSize)
+            zoomFactor /= screenMultiplierForGridSize
+
             initColors()
+            centerView()
             initParticles()
             rulesMatrix = makeRandomMatrix()
             console.table(rulesMatrix)
+        }
+        function centerView() {
+            const centerX = canvasWidth / 2 / zoomFactor - gridOffsetX
+            const centerY = canvasHeight / 2 / zoomFactor - gridOffsetY
+            const offsetX = gridWidth / 2 - centerX
+            const offsetY = gridHeight / 2 - centerY
+            gridOffsetX -= offsetX
+            gridOffsetY -= offsetY
+            setEndCoordinates()
         }
         function initColors() {
             currentColors = [];
@@ -126,9 +145,9 @@ export default defineComponent({
         function initParticles() {
             for (let i = 0; i < numParticles; ++i) {
                 colors[i] = currentColors[Math.floor(Math.random() * numColors)]
-                positionX[i] = Math.random() * canvasWidth
-                positionY[i] = Math.random() * canvasHeight
-                positionZ[i] = Math.random() * randomZDepth
+                positionX[i] = Math.random() * gridWidth
+                positionY[i] = Math.random() * gridHeight
+                positionZ[i] = Math.random() * (maxZDepthRandomParticle - minZDepthRandomParticle) + minZDepthRandomParticle
             }
         }
         function makeRandomMatrix() {
@@ -152,18 +171,27 @@ export default defineComponent({
             requestAnimationFrame(update)
         }
         function draw(x: number, y: number, z: number, color: number, size: number) {
-            const depthFactor = 1 - z / canvasHeight * zoomFactor // Adjust this factor to control the depth effect
+            const depthFactor = 1 - z / gridHeight / zoomFactor // Adjust this factor to control the depth effect
             const newSize = size * depthFactor * zoomFactor
-            if (newSize <= 0) return
-            ctx!.fillStyle = colorList[color]
-            ctx!.beginPath()
+            if (newSize <= 0) return // Skip if the particle is too small
+            const drawX = (x + gridOffsetX) * zoomFactor
+            const drawY = (y + gridOffsetY) * zoomFactor
+            if (drawX < 0 || drawX > canvasWidth || drawY < 0 || drawY > canvasHeight) return // Skip if the particle is outside the canvas
 
-            const drawX = (x + cameraOffsetX) * zoomFactor
-            const drawY = (y + cameraOffsetY) * zoomFactor
+            // Opacity depth effect
+            const maxOpacity = 1
+            const minOpacity = 0.5 // Depth effect will be stronger with lower opacity
+            const opacity = depthLimit > 0 ? maxOpacity - (z / depthLimit) * (maxOpacity - minOpacity) : 1
+            ctx!.fillStyle = `hsl(${color * 360 / numColors}, 100%, 50%, ${opacity})`
 
-            ctx!.fillRect(drawX, drawY, newSize, newSize)
-            // ctx!.arc(x + cameraOffsetX, y + cameraOffsetY, newSize, 0, Math.PI * 2)
-            // ctx!.fill()
+            // ctx!.fillStyle = colorList[color]
+            if (newSize <= 2) { // Draw squares for small particles
+                ctx!.fillRect(drawX, drawY, newSize, newSize)
+            } else { // Draw circles for larger particles
+                ctx!.beginPath()
+                ctx!.arc(drawX, drawY, newSize / 2, 0, Math.PI * 2)
+                ctx!.fill()
+            }
         }
         function getForce(ruleFactor: number, distance: number) {
             if (distance < minRadius) {
@@ -191,7 +219,6 @@ export default defineComponent({
                 if (!cells.has(cellKey)) {
                     cells.set(cellKey, [])
                 }
-
                 cells.get(cellKey)!.push(i)
             }
 
@@ -216,7 +243,6 @@ export default defineComponent({
                             if (!cells.has(neighborKey)) {
                                 continue
                             }
-
                             const neighbors = cells.get(neighborKey)!
 
                             // Process each particle in the neighboring cell
@@ -269,17 +295,17 @@ export default defineComponent({
                 positionZ[i] += velocityZ[i]
 
                 // // Bounce off the walls
-                // if (positionX[i] > canvasWidth || positionX[i] < 0) {
-                //     positionX[i] -= velocityX[i];
-                //     velocityX[i] *= -1;
-                // }
-                // if (positionY[i] > canvasHeight || positionY[i] < 0) {
-                //     positionY[i] -= velocityY[i];
-                //     velocityY[i] *= -1;
-                // }
-                if (positionZ[i] > maxZ || positionZ[i] < minZ) {
-                    positionZ[i] -= velocityZ[i];
-                    velocityZ[i] *= -1;
+                if (positionX[i] > gridWidth || positionX[i] < 0) {
+                    positionX[i] -= velocityX[i]
+                    velocityX[i] *= -1
+                }
+                if (positionY[i] > gridHeight || positionY[i] < 0) {
+                    positionY[i] -= velocityY[i]
+                    velocityY[i] *= -1
+                }
+                if (positionZ[i] > depthLimit || positionZ[i] < 0) {
+                    positionZ[i] -= velocityZ[i]
+                    velocityZ[i] *= -1
                 }
 
                 // // Wrap around the screen
@@ -302,25 +328,25 @@ export default defineComponent({
         // -------------------------------------------------------------------------------------------------------------
         function drawGrid() {
             ctx!.beginPath()
-            drawHorizontalLine(endHorizontalX, cameraOffsetY * zoomFactor) // Draw top line
-            drawHorizontalLine(endHorizontalX, (cameraOffsetY + canvasHeight) * zoomFactor) // Draw bottom line
-            drawVerticalLine(cameraOffsetX * zoomFactor, endVerticalY) // Draw left line
-            drawVerticalLine((cameraOffsetX + canvasWidth) * zoomFactor, endVerticalY) // Draw right line
+            drawHorizontalLine(endGridX, gridOffsetY * zoomFactor) // Draw top line
+            drawHorizontalLine(endGridX, (gridOffsetY + gridHeight) * zoomFactor) // Draw bottom line
+            drawVerticalLine(gridOffsetX * zoomFactor, endGridY) // Draw left line
+            drawVerticalLine((gridOffsetX + gridWidth) * zoomFactor, endGridY) // Draw right line
             ctx!.strokeStyle = 'rgba(255, 255, 255, 0.8)'
             ctx!.lineWidth = 1
             ctx!.stroke()
         }
         function drawHorizontalLine(x: number, y: number) {
-            ctx!.moveTo(cameraOffsetX * zoomFactor, y)
+            ctx!.moveTo(gridOffsetX * zoomFactor, y)
             ctx!.lineTo(x, y)
         }
         function drawVerticalLine(x: number, y: number) {
-            ctx!.moveTo(x, cameraOffsetY * zoomFactor)
+            ctx!.moveTo(x, gridOffsetY * zoomFactor)
             ctx!.lineTo(x, y)
         }
         function setEndCoordinates() {
-            endHorizontalX = cameraOffsetX * zoomFactor + canvasWidth * zoomFactor
-            endVerticalY = cameraOffsetY * zoomFactor + canvasHeight * zoomFactor
+            endGridX = gridOffsetX * zoomFactor + gridWidth * zoomFactor
+            endGridY = gridOffsetY * zoomFactor + gridHeight * zoomFactor
         }
 
         return { lifeCanvas }
