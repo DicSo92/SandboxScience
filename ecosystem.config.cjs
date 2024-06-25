e9adbb04f4c7d881160e7c3bb3cebc1995b49b31
Luzzi Charly <luzzi.charly@gmail.com>
2024-06-25 03:52:28 +0200

    Add pm2 log files

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,7 +5,13 @@ module.exports = {
             port: '3000',
             exec_mode: 'cluster',
             instances: 'max',
-            script: './.output/server/index.mjs'
+            script: './.output/server/index.mjs',
+            args: 'start',
+
+            log_file: '/var/log/pm2/sandbox-science.log',
+            error_file: '/var/log/pm2/sandbox-science-error.log',
+            out_file: '/var/log/pm2/sandbox-science-out.log',
+            merge_logs: true,
         }
     ]
 }
