5148203be84c478131adc22220b72f8eb727a445
Luzzi Charly <luzzi.charly@gmail.com>
2024-08-11 16:51:08 +0200

    Add custom favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,14 +25,20 @@ export default defineNuxtConfig({
         head: {
             viewport: 'width=device-width,initial-scale=1',
             link: [
-                { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
-                // { rel: 'icon', type: 'image/svg+xml', href: '/nuxt.svg' },
-                // { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+                { rel: 'icon', href: '/favicon.ico?v=1', sizes: 'any' },
+                { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png?v=1' },
+                { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png?v=1' },
+                { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png?v=1' },
+                { rel: 'manifest', href: '/site.webmanifest?v=1' },
+                { rel: 'mask-icon', type: 'image/svg+xml', href: '/safari-pinned-tab.svg?v=1', color: '#264653' },
+                { rel: 'shortcut icon', href: '/favicon.ico?v=1' },
             ],
             meta: [
                 { name: 'viewport', content: 'width=device-width, initial-scale=1' },
                 { name: 'description', content: appDescription },
                 { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+                { name: 'msapplication-TileColor', content: '#264653' },
+                { name: 'theme-color', content: '#264653' },
             ],
         },
     },
