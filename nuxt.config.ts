e09a21bef63a64c826bc3d3182f9afd65e3ebd0b
Luzzi Charly <luzzi.charly@gmail.com>
2025-10-12 05:32:22 +0200

    Disables canonical site URL redirection
    Disables the redirection to the canonical site URL.
    This change provides more flexibility in managing different versions or deployments of the site, as it prevents automatic redirection to a single canonical URL.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -59,7 +59,7 @@ export default defineNuxtConfig({
         },
     },
     seo: {
-        redirectToCanonicalSiteUrl: true
+        redirectToCanonicalSiteUrl: false
     },
     schemaOrg: {
         identity: {
